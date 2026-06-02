2e029638e4a7e1f5693da1435cd12d44aba0f2c5
GiovaneVesentini <giovanevicentine@gmail.com>
2026-06-02 07:53:06 -0300

    mudar atributos para protected e declarar GC

--- Fase.h
+++ Fase.h
@@ -2,22 +2,22 @@
 #include "Jogador.h"
 #include "Inimigo.h"
 #include "ListaEntidades.h"
+#include "GerenciadorColisao.h"
 #include <SFML/Graphics.hpp>
 #include<optional>
 
 class Fase
 {
-private:
+protected:
 	ListaEntidades* listaEntidades;
 	Jogador* j1;
 	Inimigo* i1;
-	sf::RenderWindow* window;
+	GerenciadorColisao GC;
+	sf::RenderWindow* window;//usar gerenciador gráfico
 	void inicializaElementos();
 public:
 	Fase(Jogador* j1, sf::RenderWindow* window);
 	~Fase();
 	ListaEntidades* getListaEntidades() { return listaEntidades; }
-
-
 };
 

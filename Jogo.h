98dc4fdf6beaa318e527795792f8d8b9dd663dae
GiovaneVesentini <giovanevicentine@gmail.com>
2026-06-02 07:47:03 -0300

    Comentários a respeito da implementação do Gerenciador de Colisão na classe Jogo

--- Jogo.h
+++ Jogo.h
@@ -16,7 +16,7 @@ private:
     Jogador* player1;//aqui deve ser pJogador1
     ListaEntidades* LEs;//classe Lista e ListaEntidades é declarada dentro da classe Fase não em Jogo
     Fase* fase1;//a classe jogo chama Fase_prim ou Fase_seg direto não a classe base Fase
-    GerenciadorColisao* pGC;//na classe jogo não se declara como ponteiro , ajustar
+    GerenciadorColisao* pGC;//na classe jogo não se declara Gerenciador de colisões apenas na classe fase
 	
 public:
     Jogo();

0839cfc1b6f66adc29582bbc20a90cd66a4dd1e6
GiovaneVesentini <giovanevicentine@gmail.com>
2026-06-02 16:23:32 -0300

    Comentarios sobre gerenciador grafico, colidir e adicionei include Ente

--- Entidade.h
+++ Entidade.h
@@ -1,19 +1,20 @@
 #pragma once
 #include<SFML/Graphics.hpp>
+#include "Ente.h"
 #include<optional>  
 
 class Entidade
 {
 protected:
 	sf::RectangleShape body;
-	sf::RenderWindow* window;
+	sf::RenderWindow* window;//usar no gerenciador grafico
 public:
 	Entidade();
 	~Entidade();
 
 	sf::FloatRect getBounds();
-	virtual void colidir(Entidade* e);
-	void SetWindow(sf::RenderWindow * window) {this->window = window;}
-	void Draw() { window->draw(body); }
+	virtual void colidir(Entidade* e);// as entidades se colidem gerenciador só verifica e usa essa função em tratar colisões
+	void SetWindow(sf::RenderWindow * window) {this->window = window;}//competencia do gerenciador grafico
+	void Draw() { window->draw(body); }// entidade não tem a função desenhar , sua classe base tem
 };
 

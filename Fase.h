74667bbc198a9c8c852ca3555195ac6ac025f8fe
GiovaneVesentini <giovanevicentine@gmail.com>
2026-06-02 17:37:31 -0300

    Mudança de acesso das variaiveis e comentarios a respeito de entidades

--- Fase.h
+++ Fase.h
@@ -3,21 +3,23 @@
 #include "Inimigo.h"
 #include "ListaEntidades.h"
 #include <SFML/Graphics.hpp>
+#include "GerenciadorColisao"
 #include<optional>
 
 class Fase
 {
-private:
-	ListaEntidades* listaEntidades;
+protected:
+	ListaEntidades* listaEntidades;//não é ponteiro para lista entidades
 	Jogador* j1;
-	Inimigo* i1;
+	Inimigo* i1;//acesso jogador e inimigo pela lista de entidades
+	GerenciadorColisao GC;
 	sf::RenderWindow* window;
 	void inicializaElementos();
+	void criarCenario();
 public:
 	Fase(Jogador* j1, sf::RenderWindow* window);
 	~Fase();
 	ListaEntidades* getListaEntidades() { return listaEntidades; }
-
-
+	virtual void executar()=0;
 };
 
